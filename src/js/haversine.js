70f4e49fcd71d52005612523264ebf1db1444c8e
bcaller <bcaller@users.noreply.github.com>
2016-07-07 01:56:27 +0200

    Silly cloudpebble
    It didn't save the changes

--- src/js/haversine.js
+++ src/js/haversine.js
@@ -1,12 +1,2 @@
 var LatLon = require('geodesy-spherical');
-
-var ll = module.exports = function(z) { return new LatLon(z.latitude||z.lat, z.longitude||z.lon); }
-
-function bearing(dest, orig) {
-	return ll(orig).bearingTo(ll(dest));
-}
-
-module.exports.dist = function distance(z, q) {
-    return ll(z).distanceTo(ll(q));
-};
-module.exports.bearing = bearing;
+module.exports = function(z) { return new LatLon(z.latitude||z.lat, z.longitude||z.lon); }
