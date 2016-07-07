a25c84f4396d12c5a1671a0c906b329c0856c800
bcaller <bcaller@users.noreply.github.com>
2016-07-07 17:39:01 +0200

    There should be a license

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Point compass to nearest bike / station with availability
 
 [Get on Pebble App Store](https://apps.getpebble.com/en_US/application/5717f5d4c883965d2f000016)
 
+## Info
+Data comes from [pybikes](https://github.com/eskerda/pybikes) via the [citybik.es API](https://citybik.es).
+Pebble C and JS code licensed under GPLv3.
+
 ![screenshot](https://assets.getpebble.com/api/file/PtA8mgz6SeCwCGVslute/convert?cache=true&fit=crop&w=144&h=168) ![screenshot](https://assets.getpebble.com/api/file/8o5OIVXRD6jqxr3JsyHy/convert?cache=true&fit=crop&w=144&h=168)
--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/*
+    Bike Sharer for Pebble: shows the location of nearby rental bikes
+    Copyright (C) 2016  Ben Caller
+    Data provided by citybik.es
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <pebble.h>
 #include "bikeorparking.h"
 #include "list.h"
